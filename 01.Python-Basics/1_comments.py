4901b82ec15d6be36d58255eccda9024ae9b62d0
abdussalam02 <samaniabdussalam02@gmail.com>
2021-12-28 23:57:36 +0530

    comments explained

---
+++ 01.Python-Basics/1_comments.py
@@ -0,0 +1,24 @@
+# Comments in Python
+
+# After # everything that you write on that line will be skipped by the interpreter
+
+# This is a single line comment
+
+greet = "Hello Stranger" # You can comment on every line
+quest = 'How are you doing..?' # You can assign strings using single quote and double quotes, your choice
+print(greet) # we are printing our value
+print(quest)
+
+"""This is a multiline comment,
+   Let me introduce myself,
+   I am Abdus Salam learning like you guys 
+   and i am quite sure that this journey of learning
+   will never going to end untill death takes place"""
+
+# You can use single quote or double quotes depends on you
+
+'''This is a multiline comment,
+   Let me introduce myself,
+   I am Abdus Salam learning like you guys 
+   and i am quite sure that this journey of learning
+   will never going to end untill death takes place'''

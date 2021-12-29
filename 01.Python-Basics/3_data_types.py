962df196e2a859c199d11ca14b576aae3550e791
abdussalam02 <samaniabdussalam02@gmail.com>
2021-12-30 00:00:13 +0530

    type printed

--- 01.Python-Basics/3_data_types.py
+++ 01.Python-Basics/3_data_types.py
@@ -16,25 +16,37 @@ b = 60
 print(type(b))
 
 c = 20.5
+print(type(b))
 
 d = 1j
+print(type(b))
 
 e = ["apple", "banana", "cherry"]	
+print(type(b))
 
 f = ("apple", "banana", "cherry")
+print(type(b))
 
 g = range(6)
+print(type(b))
 
 h = {"name" : "John", "age" : 36}
+print(type(b))
 
 i = {"apple", "banana", "cherry"}
+print(type(b))
 
 j = frozenset({"apple", "banana", "cherry"})
+print(type(b))
 
 k = True
+print(type(b))
 
 l = b"Hello"
+print(type(b))
 
 m = bytearray(5)
+print(type(b))
 
 n = memoryview(bytes(5))
+print(type(b))

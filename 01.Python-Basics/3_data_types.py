cbf87c28b8511931cefc55257c7588ed62d7cea7
abdussalam02 <samaniabdussalam02@gmail.com>
2021-12-30 00:08:31 +0530

    changes

--- 01.Python-Basics/3_data_types.py
+++ 01.Python-Basics/3_data_types.py
@@ -9,44 +9,45 @@ Set Types:	set, frozenset e.g(set = (), frozenset = ?)
 Boolean Type:	bool e.g (True, False)
 Binary Types:	bytes, bytearray, memoryview e.g(.........)
 '''
-a = "Hello World"
-print(type(a))
+
+a = 'Hello World'
+print("'Hello World' is of type ", type(a))
 
 b = 60
-print(type(b))
+print(" 60 is of type ", type(b))
 
 c = 20.5
-print(type(b))
+print(type(c))
 
 d = 1j
-print(type(b))
+print(type(d))
 
 e = ["apple", "banana", "cherry"]	
-print(type(b))
+print(type(e))
 
 f = ("apple", "banana", "cherry")
-print(type(b))
+print(type(f))
 
 g = range(6)
-print(type(b))
+print(type(g))
 
 h = {"name" : "John", "age" : 36}
-print(type(b))
+print(type(h))
 
 i = {"apple", "banana", "cherry"}
-print(type(b))
+print(type(i))
 
 j = frozenset({"apple", "banana", "cherry"})
-print(type(b))
+print(type(j))
 
 k = True
-print(type(b))
+print(type(k))
 
 l = b"Hello"
-print(type(b))
+print(type(l))
 
 m = bytearray(5)
-print(type(b))
+print(type(m))
 
 n = memoryview(bytes(5))
-print(type(b))
+print(type(n))
